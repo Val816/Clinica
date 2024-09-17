778022e32ca484ab914f12104b9f82e58d10727b
VALERIA LOPEZ CISNEROS <20050200@saltillo.tecnm.mx>
2024-09-17 10:57:56 -0600

    Interfaz de login

---
+++ Clinica/Login.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+	Inherits System.Windows.Forms.Form
+
+	'Form reemplaza a Dispose para limpiar la lista de componentes.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Requerido por el Diseñador de Windows Forms
+	Private components As System.ComponentModel.IContainer
+
+	'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+	'Se puede modificar usando el Diseñador de Windows Forms.  
+	'No lo modifique con el editor de código.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
+		Me.TextBox2 = New System.Windows.Forms.TextBox()
+		Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+		Me.SuspendLayout()
+		'
+		'Button1
+		'
+		Me.Button1.Location = New System.Drawing.Point(208, 253)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(75, 23)
+		Me.Button1.TabIndex = 0
+		Me.Button1.Text = "Ingresar"
+		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(201, 62)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(82, 13)
+		Me.Label1.TabIndex = 1
+		Me.Label1.Text = "Inicio de Sesión"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Location = New System.Drawing.Point(139, 113)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(96, 13)
+		Me.Label2.TabIndex = 2
+		Me.Label2.Text = "Correo electrónico:"
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Location = New System.Drawing.Point(171, 195)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(64, 13)
+		Me.Label3.TabIndex = 3
+		Me.Label3.Text = "Contraseña:"
+		'
+		'TextBox1
+		'
+		Me.TextBox1.Location = New System.Drawing.Point(241, 106)
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(126, 20)
+		Me.TextBox1.TabIndex = 4
+		'
+		'TextBox2
+		'
+		Me.TextBox2.Location = New System.Drawing.Point(241, 188)
+		Me.TextBox2.Name = "TextBox2"
+		Me.TextBox2.Size = New System.Drawing.Size(126, 20)
+		Me.TextBox2.TabIndex = 5
+		'
+		'LinkLabel1
+		'
+		Me.LinkLabel1.AutoSize = True
+		Me.LinkLabel1.Location = New System.Drawing.Point(184, 295)
+		Me.LinkLabel1.Name = "LinkLabel1"
+		Me.LinkLabel1.Size = New System.Drawing.Size(113, 13)
+		Me.LinkLabel1.TabIndex = 6
+		Me.LinkLabel1.TabStop = True
+		Me.LinkLabel1.Text = "Recuperar contraseña"
+		'
+		'Login
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(496, 355)
+		Me.Controls.Add(Me.LinkLabel1)
+		Me.Controls.Add(Me.TextBox2)
+		Me.Controls.Add(Me.TextBox1)
+		Me.Controls.Add(Me.Label3)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.Button1)
+		Me.Name = "Login"
+		Me.Text = "Login"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents Button1 As Button
+	Friend WithEvents Label1 As Label
+	Friend WithEvents Label2 As Label
+	Friend WithEvents Label3 As Label
+	Friend WithEvents TextBox1 As TextBox
+	Friend WithEvents TextBox2 As TextBox
+	Friend WithEvents LinkLabel1 As LinkLabel
+End Class
