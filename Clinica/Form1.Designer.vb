50417ef44f490c2a179d32c3bcd4616f395900ac
VALERIA LOPEZ CISNEROS <20050200@saltillo.tecnm.mx>
2024-09-17 12:22:53 -0600

    Interfaz de login

--- Clinica/Form1.Designer.vb
+++ Clinica/Form1.Designer.vb
@@ -23,28 +23,98 @@ Partial Class Form1
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
 		Me.Button1 = New System.Windows.Forms.Button()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
+		Me.TextBox2 = New System.Windows.Forms.TextBox()
 		Me.SuspendLayout()
 		'
 		'Button1
 		'
-		Me.Button1.Location = New System.Drawing.Point(152, 145)
+		Me.Button1.Location = New System.Drawing.Point(193, 255)
 		Me.Button1.Name = "Button1"
 		Me.Button1.Size = New System.Drawing.Size(75, 23)
 		Me.Button1.TabIndex = 0
-		Me.Button1.Text = "Button1"
+		Me.Button1.Text = "Ingresar"
 		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(200, 57)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(82, 13)
+		Me.Label1.TabIndex = 1
+		Me.Label1.Text = "Inicio de Sesión"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Location = New System.Drawing.Point(113, 124)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(96, 13)
+		Me.Label2.TabIndex = 2
+		Me.Label2.Text = "Correo electrónico:"
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Location = New System.Drawing.Point(145, 202)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(64, 13)
+		Me.Label3.TabIndex = 3
+		Me.Label3.Text = "Contraseña:"
+		'
+		'LinkLabel1
+		'
+		Me.LinkLabel1.AutoSize = True
+		Me.LinkLabel1.Location = New System.Drawing.Point(169, 305)
+		Me.LinkLabel1.Name = "LinkLabel1"
+		Me.LinkLabel1.Size = New System.Drawing.Size(113, 13)
+		Me.LinkLabel1.TabIndex = 4
+		Me.LinkLabel1.TabStop = True
+		Me.LinkLabel1.Text = "Recuperar contraseña"
+		'
+		'TextBox1
+		'
+		Me.TextBox1.Location = New System.Drawing.Point(215, 117)
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(159, 20)
+		Me.TextBox1.TabIndex = 5
+		'
+		'TextBox2
+		'
+		Me.TextBox2.Location = New System.Drawing.Point(215, 195)
+		Me.TextBox2.Name = "TextBox2"
+		Me.TextBox2.Size = New System.Drawing.Size(159, 20)
+		Me.TextBox2.TabIndex = 6
 		'
 		'Form1
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(800, 450)
+		Me.ClientSize = New System.Drawing.Size(490, 374)
+		Me.Controls.Add(Me.TextBox2)
+		Me.Controls.Add(Me.TextBox1)
+		Me.Controls.Add(Me.LinkLabel1)
+		Me.Controls.Add(Me.Label3)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Label1)
 		Me.Controls.Add(Me.Button1)
 		Me.Name = "Form1"
-		Me.Text = "Form1"
+		Me.Text = "Login"
 		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
 	End Sub
 
 	Friend WithEvents Button1 As Button
+	Friend WithEvents Label1 As Label
+	Friend WithEvents Label2 As Label
+	Friend WithEvents Label3 As Label
+	Friend WithEvents LinkLabel1 As LinkLabel
+	Friend WithEvents TextBox1 As TextBox
+	Friend WithEvents TextBox2 As TextBox
 End Class
