d369165af5c1af25e18192f0d7ac381346a1ff2b
VALERIA LOPEZ CISNEROS <20050200@saltillo.tecnm.mx>
2024-09-23 07:52:45 -0600

    Inicio de sesion lista

--- Clinica/Inicio de sesion.Designer.vb
+++ Clinica/Inicio de sesion.Designer.vb
@@ -22,155 +22,166 @@ Partial Class Inicio_de_sesion
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Inicio_de_sesion))
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
-        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.Lime
-        Me.Label1.Location = New System.Drawing.Point(524, 51)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(210, 46)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "VETCARE"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(509, 277)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(238, 36)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Inicio de sesión"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(436, 347)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(171, 25)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Correo electrónico"
-        '
-        'TextBox1
-        '
-        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(441, 385)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(411, 30)
-        Me.TextBox1.TabIndex = 3
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(436, 452)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(114, 25)
-        Me.Label4.TabIndex = 4
-        Me.Label4.Text = "Contraseña"
-        '
-        'TextBox2
-        '
-        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox2.Location = New System.Drawing.Point(441, 489)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(411, 30)
-        Me.TextBox2.TabIndex = 5
-        '
-        'Button1
-        '
-        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(197, Byte), Integer), CType(CType(225, Byte), Integer), CType(CType(165, Byte), Integer))
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(441, 568)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(411, 43)
-        Me.Button1.TabIndex = 6
-        Me.Button1.Text = "Ingresar"
-        Me.Button1.UseVisualStyleBackColor = False
-        '
-        'LinkLabel1
-        '
-        Me.LinkLabel1.ActiveLinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.LinkLabel1.AutoSize = True
-        Me.LinkLabel1.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.LinkLabel1.Location = New System.Drawing.Point(582, 655)
-        Me.LinkLabel1.Name = "LinkLabel1"
-        Me.LinkLabel1.Size = New System.Drawing.Size(140, 20)
-        Me.LinkLabel1.TabIndex = 7
-        Me.LinkLabel1.TabStop = True
-        Me.LinkLabel1.Text = "Recuperar Cuenta"
-        '
-        'LinkLabel2
-        '
-        Me.LinkLabel2.ActiveLinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.LinkLabel2.AutoSize = True
-        Me.LinkLabel2.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.LinkLabel2.Location = New System.Drawing.Point(591, 696)
-        Me.LinkLabel2.Name = "LinkLabel2"
-        Me.LinkLabel2.Size = New System.Drawing.Size(104, 20)
-        Me.LinkLabel2.TabIndex = 8
-        Me.LinkLabel2.TabStop = True
-        Me.LinkLabel2.Text = "Crear Cuenta"
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(573, 94)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(107, 180)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.PictureBox1.TabIndex = 9
-        Me.PictureBox1.TabStop = False
-        '
-        'Inicio_de_sesion
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.ClientSize = New System.Drawing.Size(1277, 799)
-        Me.Controls.Add(Me.PictureBox1)
-        Me.Controls.Add(Me.LinkLabel2)
-        Me.Controls.Add(Me.LinkLabel1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Name = "Inicio_de_sesion"
-        Me.Text = "Inicio_de_sesion"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Inicio_de_sesion))
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.txtCorreo = New System.Windows.Forms.TextBox()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.txtContraseña = New System.Windows.Forms.TextBox()
+		Me.btnIngresar = New System.Windows.Forms.Button()
+		Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+		Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.ForeColor = System.Drawing.Color.Lime
+		Me.Label1.Location = New System.Drawing.Point(349, 33)
+		Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(143, 31)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "VETCARE"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label2.Location = New System.Drawing.Point(339, 180)
+		Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(162, 25)
+		Me.Label2.TabIndex = 1
+		Me.Label2.Text = "Inicio de sesión"
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label3.Location = New System.Drawing.Point(291, 226)
+		Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(124, 17)
+		Me.Label3.TabIndex = 2
+		Me.Label3.Text = "Correo electrónico"
+		'
+		'txtCorreo
+		'
+		Me.txtCorreo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.txtCorreo.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.txtCorreo.Location = New System.Drawing.Point(294, 250)
+		Me.txtCorreo.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+		Me.txtCorreo.Name = "txtCorreo"
+		Me.txtCorreo.Size = New System.Drawing.Size(275, 23)
+		Me.txtCorreo.TabIndex = 3
+		'
+		'Label4
+		'
+		Me.Label4.AutoSize = True
+		Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label4.Location = New System.Drawing.Point(291, 294)
+		Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(81, 17)
+		Me.Label4.TabIndex = 4
+		Me.Label4.Text = "Contraseña"
+		'
+		'txtContraseña
+		'
+		Me.txtContraseña.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.txtContraseña.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.txtContraseña.Location = New System.Drawing.Point(294, 318)
+		Me.txtContraseña.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+		Me.txtContraseña.Name = "txtContraseña"
+		Me.txtContraseña.Size = New System.Drawing.Size(275, 23)
+		Me.txtContraseña.TabIndex = 5
+		'
+		'btnIngresar
+		'
+		Me.btnIngresar.BackColor = System.Drawing.Color.FromArgb(CType(CType(197, Byte), Integer), CType(CType(225, Byte), Integer), CType(CType(165, Byte), Integer))
+		Me.btnIngresar.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.btnIngresar.Location = New System.Drawing.Point(294, 369)
+		Me.btnIngresar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+		Me.btnIngresar.Name = "btnIngresar"
+		Me.btnIngresar.Size = New System.Drawing.Size(274, 28)
+		Me.btnIngresar.TabIndex = 6
+		Me.btnIngresar.Text = "Ingresar"
+		Me.btnIngresar.UseVisualStyleBackColor = False
+		'
+		'LinkLabel1
+		'
+		Me.LinkLabel1.ActiveLinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+		Me.LinkLabel1.AutoSize = True
+		Me.LinkLabel1.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+		Me.LinkLabel1.Location = New System.Drawing.Point(388, 426)
+		Me.LinkLabel1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.LinkLabel1.Name = "LinkLabel1"
+		Me.LinkLabel1.Size = New System.Drawing.Size(94, 13)
+		Me.LinkLabel1.TabIndex = 7
+		Me.LinkLabel1.TabStop = True
+		Me.LinkLabel1.Text = "Recuperar Cuenta"
+		'
+		'LinkLabel2
+		'
+		Me.LinkLabel2.ActiveLinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+		Me.LinkLabel2.AutoSize = True
+		Me.LinkLabel2.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+		Me.LinkLabel2.Location = New System.Drawing.Point(394, 452)
+		Me.LinkLabel2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.LinkLabel2.Name = "LinkLabel2"
+		Me.LinkLabel2.Size = New System.Drawing.Size(69, 13)
+		Me.LinkLabel2.TabIndex = 8
+		Me.LinkLabel2.TabStop = True
+		Me.LinkLabel2.Text = "Crear Cuenta"
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+		Me.PictureBox1.Location = New System.Drawing.Point(382, 61)
+		Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(71, 117)
+		Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+		Me.PictureBox1.TabIndex = 9
+		Me.PictureBox1.TabStop = False
+		'
+		'Inicio_de_sesion
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+		Me.ClientSize = New System.Drawing.Size(851, 487)
+		Me.Controls.Add(Me.PictureBox1)
+		Me.Controls.Add(Me.LinkLabel2)
+		Me.Controls.Add(Me.LinkLabel1)
+		Me.Controls.Add(Me.btnIngresar)
+		Me.Controls.Add(Me.txtContraseña)
+		Me.Controls.Add(Me.Label4)
+		Me.Controls.Add(Me.txtCorreo)
+		Me.Controls.Add(Me.Label3)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.Label1)
+		Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+		Me.Name = "Inicio_de_sesion"
+		Me.Text = "Inicio_de_sesion"
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
-    End Sub
+	End Sub
 
-    Friend WithEvents Label1 As Label
+	Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtCorreo As TextBox
     Friend WithEvents Label4 As Label
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents txtContraseña As TextBox
+    Friend WithEvents btnIngresar As Button
     Friend WithEvents LinkLabel1 As LinkLabel
     Friend WithEvents LinkLabel2 As LinkLabel
     Friend WithEvents PictureBox1 As PictureBox
--- Clinica/Inicio de sesion.vb
+++ Clinica/Inicio de sesion.vb
@@ -1,9 +1,57 @@
-﻿Public Class Inicio_de_sesion
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+﻿Imports MySql.Data.MySqlClient
+Public Class Inicio_de_sesion
 
-    End Sub
 
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+    Public Class Form1
+        ' Cadena de conexión, ajusta esto de acuerdo a tu configuración de base de datos
+        Dim connectionString As String = "Server=localhost;Database=veterinaria;User Id=root;Password=root;"
 
-    End Sub
+        ' Evento de clic en el botón "Ingresar"
+        Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+            ' Obtener los valores ingresados
+            Dim correo As String = txtCorreo.Text
+            Dim contraseña As String = txtContraseña.Text
+
+            ' Llamar a la función para verificar el inicio de sesión
+            If VerificarCredenciales(correo, contraseña) Then
+                MessageBox.Show("Inicio de sesión exitoso.")
+                ' Aquí puedes agregar el código para abrir la siguiente ventana o formulario
+            Else
+                MessageBox.Show("Correo o contraseña incorrectos.")
+            End If
+        End Sub
+
+        ' Función para verificar credenciales en la base de datos
+        Private Function VerificarCredenciales(correo As String, contraseña As String) As Boolean
+            Dim resultado As Boolean = False
+
+            ' Crear una conexión con la base de datos
+            Using connection As New MySqlConnection(connectionString)
+                Try
+                    connection.Open()
+
+                    ' Crear el comando SQL para buscar el usuario
+                    Dim query As String = "SELECT COUNT(*) FROM Usuario WHERE Correo = @correo AND Contraseña = @contraseña"
+                    Using command As New MySqlCommand(query, connection)
+                        ' Agregar los parámetros
+                        command.Parameters.AddWithValue("@correo", correo)
+                        command.Parameters.AddWithValue("@contraseña", contraseña)
+
+                        ' Ejecutar el comando y obtener el resultado
+                        Dim count As Integer = Convert.ToInt32(command.ExecuteScalar())
+
+                        ' Si el conteo es mayor a 0, el usuario existe
+                        If count > 0 Then
+                            resultado = True
+                        End If
+                    End Using
+                Catch ex As Exception
+                    MessageBox.Show("Error al conectar con la base de datos: " & ex.Message)
+                End Try
+            End Using
+
+            Return resultado
+        End Function
+    End Class
+
 End Class
